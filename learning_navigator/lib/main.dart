08484828edee05f221ab4271116fbf72f4bd2b32
clairtonluz <clairton.c.l@gmail.com>
2018-12-10 16:41:41 -0300

    animação Hero entre telas requisição imagem na rede

--- learning_navigator/lib/main.dart
+++ learning_navigator/lib/main.dart
@@ -1,106 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HeroApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HeroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstScreen(),
+      title: 'Transition Demo',
+      home: MainScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Screen'),
+        title: Text('Main Screen'),
       ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(todos[index].title),
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(todo: todos[index],)));
-              },
-            );
-          },
+      body: GestureDetector(
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+            'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
+          ),
         ),
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return DetailScreen();
+          }));
+        },
       ),
     );
   }
 }
 
 class DetailScreen extends StatelessWidget {
-  // Declare a field that holds the Todo
-  final Todo todo;
-
-  // In the constructor, require a Todo
-  DetailScreen({Key key, @required this.todo}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // Use the Todo to create our UI
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("${todo.title}"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text('${todo.description}'),
-      ),
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Screen"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
+            ),
+          ),
         ),
+        onTap: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
 }
-
-class Todo {
-  final String title;
-  final String description;
-
-  Todo(this.title, this.description);
-}
-
-final todos = List<Todo>.generate(
-  20,
-  (i) => Todo(
-        'Todo $i',
-        'A description of what needs to be done for Todo $i',
-      ),
-);
