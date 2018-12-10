e3b768e6b67fb6daeccf82d02176729e9beb37f0
clairtonluz <clairton.c.l@gmail.com>
2018-12-10 15:29:26 -0300

    Send data to a new screen

--- learning_navigator/lib/main.dart
+++ learning_navigator/lib/main.dart
@@ -33,15 +33,39 @@ class FirstScreen extends StatelessWidget {
         title: Text('First Screen'),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('Launch screen'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondScreen()),
+        child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(todos[index].title),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(todo: todos[index],)));
+              },
             );
           },
         ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo
+  final Todo todo;
+
+  // In the constructor, require a Todo
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create our UI
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${todo.title}"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('${todo.description}'),
       ),
     );
   }
@@ -65,3 +89,18 @@ class SecondScreen extends StatelessWidget {
     );
   }
 }
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
+
+final todos = List<Todo>.generate(
+  20,
+  (i) => Todo(
+        'Todo $i',
+        'A description of what needs to be done for Todo $i',
+      ),
+);
