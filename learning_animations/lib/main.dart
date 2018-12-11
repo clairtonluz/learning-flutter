bd2044e918aebd2a68fc156f96db2f445b00d152
clairtonluz <clairton.c.l@gmail.com>
2018-12-11 10:32:44 -0300

    criando a primeira animação (logo aumentando e quando clica ela vai diminuindo)

---
+++ learning_animations/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class LogoApp extends StatefulWidget {
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = Tween(begin: 0.0, end: 300.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward();
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        height: animation.value,
+        width: animation.value,
+        child: GestureDetector(
+          onTap: () => controller.reverse(),
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
+
+  dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+void main() {
+  runApp(LogoApp());
+}
